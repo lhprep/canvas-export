ea0b99e18d4d0d737e6b66b2d343b4880e66a9ec
Dominic Canora <dominiccanora@gmail.com>
2021-03-12 14:31:24 -0500

    It doesn't work but I'm going home

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 import time
+import subprocess
+
+from pprint import pprint as print
 
 from canvasapi import Canvas
 
@@ -8,26 +11,38 @@ canvas = Canvas("https://lhps.instructure.com/", os.getenv("CANVAS_TOKEN"))
 terms = canvas.get_account(1).get_enrollment_terms()
 courses = []
 
+file_prefix = "2019/2020"
+
 for t in terms:
-    break
-    if "2019/2020" in str(t):
+    if "2019/2020" in str(t) and "6th - Quarter 1" in str(t):
         print(t)
-        for course in canvas.get_account(1).get_courses(enrollment_term_id=t.id):
+        for course in canvas.get_account(1).get_courses(enrollment_term_id=t.id, include=["term"]):
             courses.append(course)
 
 print(courses)
 print(len(courses))
 
-course = canvas.get_course(8537)
+# exit(0)  # TODO: Remove
 
-export = course.export_content("zip", skip_notifications=True)
-print(export)
+exports = []
+completed = []
 
-while True:
-    progress = canvas.get_progress(export.progress_url.split("/")[-1])
-    if progress.workflow_state == "completed": break
-    print(progress.completion)
+for course in courses:
+    exports.append((course, course.export_content("zip", skip_notifications=True)))
+
+while len(completed) < len(exports):
+    progresses = []
+    for course, export in exports:
+        if export not in completed:
+            progress = canvas.get_progress(export.progress_url.split("/")[-1])
+            progresses.append(str(progress.completion))
+            if progress.workflow_state == "completed":
+                export = course.get_content_export(export)
+                filename = f"{course.sis_course_id if course.sis_course_id else file_prefix} - {course.name}.zip"
+                filename = filename.replace("/", "~")
+                subprocess.run(["curl", "-o", filename, export.attachment.get("url")])
+                completed.append(export)
+
+    print(" ".join(progresses))
+    print(" ".join(completed))
     time.sleep(1)
-
-export = course.get_content_export(export)
-print(export.attachment.get("url"))
