1205c95bfbc106fc537ccc78a517e7656caf37a0
Dominic Canora <dominiccanora@gmail.com>
2021-04-08 09:29:49 -0400

    Slow but works.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import subprocess
 from pprint import pprint as print
 
 from canvasapi import Canvas
+from tqdm import tqdm
 
 canvas = Canvas("https://lhps.instructure.com/", os.getenv("CANVAS_TOKEN"))
 
@@ -14,7 +15,7 @@ courses = []
 file_prefix = "2019/2020"
 
 for t in terms:
-    if "2020/2021" in str(t) and "6th - 2020-2021" in str(t):
+    if "2020/2021" in str(t) and "6th" in str(t):
         print(t)
         for course in canvas.get_account(1).get_courses(enrollment_term_id=t.id, include=["term"]):
             courses.append(course)
@@ -27,23 +28,28 @@ print(len(courses))
 exports = []
 completed = []
 
-for course in courses:
-    exports.append((course, course.export_content("common_cartridge", skip_notifications=True)))
+print("\n\n\nStarting Exports")
+with tqdm(total=len(courses)) as pb:
+    for course in courses:
+        exports.append((course, course.export_content("common_cartridge", skip_notifications=True)))
+        pb.update()
 
-while len(completed) < len(exports):
-    progresses = []
-    for course, export in exports:
-        if export not in completed:
-            progress = canvas.get_progress(export.progress_url.split("/")[-1])
-            progresses.append(str(progress.completion))
-            if progress.workflow_state == "completed":
-                export = course.get_content_export(export)
-                filename = f"{course.sis_course_id if course.sis_course_id else file_prefix} - {course.name}.zip"
-                filename = filename.replace("/", "~")
-                subprocess.run(["curl", "-Lo", filename, export.attachment.get("url")],
-                               stdin=None, stdout=None, stderr=None)
-                completed.append(export)
+print("\n\n\nDownloading")
+with tqdm(total=len(courses)) as pb:
+    while len(completed) < len(exports):
+        progresses = []
+        for course, export in exports:
+            if export not in completed:
+                progress = canvas.get_progress(export.progress_url.split("/")[-1])
+                progresses.append(str(progress.completion))
+                if progress.workflow_state == "completed":
+                    export = course.get_content_export(export)
+                    filename = f"{course.sis_course_id if course.sis_course_id else file_prefix} - {course.name}.zip"
+                    filename = filename.replace("/", "~")
+                    subprocess.run(["curl", "-Lo", filename, export.attachment.get("url")],
+                                   stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+                    completed.append(export)
+                    pb.update()
 
-    print(" ".join(progresses))
-    # print(" ".join(completed))
-    time.sleep(1)
+        print(" ".join(progresses))
+        time.sleep(1)
