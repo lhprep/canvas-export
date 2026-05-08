1e4934f2ce3554a18368b676f4b2fc338fccaa40
Dominic Canora <dcanora@lhprep.org>
2026-05-08 12:23:49 -0400

    Add `upload.sh` script for uploading files to remote storage.

---
+++ upload.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+DATETIME="$(TZ='America/New_York' date +%Y-%m-%dT%H:%M:%S)"
+REMOTE="canvas-drive:Spring 2026/${DATETIME}"
+
+rclone mkdir "${REMOTE}"
+rclone copy . "${REMOTE}" \
+    --exclude 'env/**' \
+    --exclude '**/*.py' \
+    --progress
