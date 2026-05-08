676c9dd77ebe839908cc0453530e44a761456fbd
Dominic Canora <dcanora@lhprep.org>
2026-05-08 14:29:27 -0400

    Exclude `env` directory from uploads in `upload.sh`.

--- upload.sh
+++ upload.sh
@@ -6,6 +6,7 @@ REMOTE="canvas-drive:Spring 2026/${DATETIME}"
 
 rclone mkdir "${REMOTE}"
 rclone copy . "${REMOTE}" \
+    --exclude 'env/**' \
     --include '*.csv' \
     --include '*.json' \
     --include '*.zip' \
